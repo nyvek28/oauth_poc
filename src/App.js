6a8249ab56c4fb79dd01d1630226b254ec2cc225
andres <andres@ip-192-168-12-64.ec2.internal>
2018-03-13 15:03:16 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-
+import SocialButton from './SocialButton';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  handleSocialLogin(user) {
+    console.log(user);
+  }
+
+  handleSocialLoginFailure(err) {
+    console.log(err);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +20,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
-        <button>Login with Google</button>
+        <SocialButton
+          provider="google"
+          appId="830991144249-dnd3pivonjjfg5mp2ark8idncvarhmmj.apps.googleusercontent.com"
+          onLoginSuccess={this.handleSocialLogin}
+          onLoginFailure={this.handleSocialLoginFailure}
+        >
+          Login with G
+        </SocialButton>
       </div>
     );
   }
